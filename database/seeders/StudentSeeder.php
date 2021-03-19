c16357accf0a5b137ef50814eb22344d1c261a55
deweks0 <lutfianjr1029f@gmail.com>
2021-03-20 00:07:52 +0700

    configure seeder

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Student;
 use Illuminate\Database\Seeder;
 
 class StudentSeeder extends Seeder
@@ -13,6 +14,11 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Student::create([
+            'user_id'=> User::whereHas('roles',function($q){return $q->where('name','student');})->first()->id,
+            'nis'=>rand(11111111,99999999),
+            'phone'=> '0895384292459',
+            'address'=>'indonesia'
+        ]);
     }
 }
