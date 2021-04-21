fe70c3fbe42a3e708321192c1d8e0d5d6a712251
deweks0 <lutfianjr1029f@gmail.com>
2021-04-21 19:31:27 +0700

    add chart to dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,56 +8,63 @@
 
         <div class="">
             <div class="max-w-7xl mx-auto lg:my-10 md:my-9 sm:my-8 lg:px-8 md:px-7 sm:px-6">
-                        
-            <div class="flex mx-auto lg:my-10 md:my-9 sm:my-8 lg:px-10 md:px-9 sm:px-8 gap-2">
 
-                <div class="flex bg-white overflow-hidden shadow-xl lg:w-1/4 lg:h-1/2 md:w-1/4 md:h-1/2 sm:w-1/5 sm:h-2/5 mx-auto lg:p-5 md:p-3.5 sm:p-2.5 rounded-xl">
-                    <div class="flex flex-row items-center">
-                        <div class="flex px-1">
-                            <div class="rounded-full lg:p-3 sm:p-0 bg-gray-200">
-                                <i class="fa fa-wallet fa-fw text-green-500"></i>
+                <div class="flex mx-auto lg:my-10 md:my-9 sm:my-8 lg:px-10 md:px-9 sm:px-8 gap-2">
+
+                    <div
+                        class="flex bg-white overflow-hidden shadow-xl lg:w-1/4 lg:h-1/2 md:w-1/4 md:h-1/2 sm:w-1/5 sm:h-2/5 mx-auto lg:p-5 md:p-3.5 sm:p-2.5 rounded-xl">
+                        <div class="flex flex-row items-center">
+                            <div class="flex px-1">
+                                <div class="rounded-full lg:p-3 sm:p-0 bg-gray-200">
+                                    <i class="fa fa-wallet fa-fw text-green-500"></i>
+                                </div>
+                            </div>
+                            <div class="flex flex-col items-center">
+                                <h3 class="font-bold lg:text-3xl sm:text-xl">{{ paymentOfTheMonth }}/{{ totalStudent }}
+                                </h3>
+                                <h5 class="font-bold text-gray-500 text-center lg:text-base sm:text-xs">Payment of The
+                                    Month</h5>
                             </div>
                         </div>
-                        <div class="flex flex-col items-center">
-                            <h3 class="font-bold lg:text-3xl sm:text-xl">{{ paymentOfTheMonth }}/{{ totalStudent }}</h3>
-                            <h5 class="font-bold text-gray-500 text-center lg:text-base sm:text-xs">Payment of The Month</h5>
-                        </div>
                     </div>
-                </div>
 
-                <div class="flex bg-white overflow-hidden shadow-xl lg:w-1/3 lg:h-1/2 sm:w-1/3 sm:h-1/2 mx-auto lg:p-8 sm:p-8 rounded-xl">
-                    <div class="flex flex-row items-center">
-                        <div class="flex-shrink pr-4">
-                            <div class="rounded-full p-3 bg-gray-200">
-                                <i class="fa fa-dollar-sign fa-fw text-green-500"></i>
+                    <div
+                        class="flex bg-white overflow-hidden shadow-xl lg:w-1/3 lg:h-1/2 sm:w-1/3 sm:h-1/2 mx-auto lg:p-8 sm:p-8 rounded-xl">
+                        <div class="flex flex-row items-center">
+                            <div class="flex-shrink pr-4">
+                                <div class="rounded-full p-3 bg-gray-200">
+                                    <i class="fa fa-dollar-sign fa-fw text-green-500"></i>
+                                </div>
+                            </div>
+                            <div class="flex flex-col items-center">
+                                <h3 class="font-bold lg:text-3xl sm:text-xl">Rp.{{ paymentOfTheYear }} <span
+                                        class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
+                                <h5 class="font-bold text-gray-500 text-center lg:text-base sm:text-xs">Payment of The
+                                    Year</h5>
                             </div>
                         </div>
-                        <div class="flex flex-col items-center">
-                            <h3 class="font-bold lg:text-3xl sm:text-xl">Rp.{{ paymentOfTheYear }} <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
-                            <h5 class="font-bold text-gray-500 text-center lg:text-base sm:text-xs">Payment of The Year</h5>
-                        </div>
                     </div>
-                </div>
 
-                <div class="flex bg-white overflow-hidden shadow-xl lg:w-1/4 lg:h-1/2 mx-auto lg:p-8 rounded-xl">
-                    <div class="flex flex-row items-center">
-                        <div class="flex-shrink pr-4">
-                            <div class="rounded-full p-3 bg-gray-200">
-                                <i class="fa fa-users fa-fw text-green-500"></i>
+                    <div class="flex bg-white overflow-hidden shadow-xl lg:w-1/4 lg:h-1/2 mx-auto lg:p-8 rounded-xl">
+                        <div class="flex flex-row items-center">
+                            <div class="flex-shrink pr-4">
+                                <div class="rounded-full p-3 bg-gray-200">
+                                    <i class="fa fa-users fa-fw text-green-500"></i>
+                                </div>
+                            </div>
+                            <div class="flex flex-col items-center mx-5">
+                                <h3 class="font-bold text-3xl">{{ totalOfficer }}</h3>
+                                <h5 class="font-bold text-gray-500 text-center lg:text-base">Officer</h5>
                             </div>
                         </div>
-                        <div class="flex flex-col items-center mx-5">
-                            <h3 class="font-bold text-3xl">{{ totalOfficer }}</h3>
-                            <h5 class="font-bold text-gray-500 text-center lg:text-base">Officer</h5>
-                        </div>
                     </div>
-                </div>
 
-            </div>
+                </div>
 
                 <div class="flex mx-auto px-10 items-center ">
                     <div class="flex bg-white overflow-hidden w-screen shadow-xl w-5/12 h-1/2 mx-auto p-8 rounded-xl">
                         <div class="flex flex-row items-center w-full justify-between">
+                            <apexchart width="500" type="bar" :options="chartOptions" :series="series"></apexchart>
                             <!-- <div class="flex items-center">
                                 <div class="flex-shrink pr-4 mx-10">
                                 <div class="rounded-full p-3 bg-gray-200">
@@ -74,11 +81,13 @@
                                 </div>
                             </div> -->
 
-                            
+
                             <div class="flex flex-row-reverse items-center">
-                                <div class="flex bg-white overflow-hidden shadow-xl w-full h-1/2 mx-auto p-8 rounded-xl">
+                                <div
+                                    class="flex bg-white overflow-hidden shadow-xl w-full h-1/2 mx-auto p-8 rounded-xl">
                                     <ul>
-                                        <li v-for="payment in payments " :key="payment.id">{{payment.student.user.name}}</li>
+                                        <li v-for="payment in payments " :key="payment.id">{{payment.student.user.name}}
+                                        </li>
                                     </ul>
                                 </div>
                             </div>
@@ -88,28 +97,70 @@
 
             </div>
         </div>
-        
+
     </app-layout>
 </template>
 
 <script>
     import AppLayout from '@/Layouts/AppLayout'
     import Card from '@/Jetstream/Card'
-    import VueApexCharts from 'vue-apexcharts'
+    import ApexCharts from 'vue3-apexcharts'
+    import numeral from 'numeral'
 
     export default {
-        props:['paymentOfTheMonth','paymentOfTheYear','totalStudent','totalOfficer','months','totalPayments','payments'],
+        props: ['paymentOfTheMonth', 'paymentOfTheYear', 'totalStudent', 'totalOfficer', 'months', 'totalPayments',
+            'payments'
+        ],
         components: {
             AppLayout,
             Card,
-            VueApexCharts
+            apexchart: ApexCharts,
         },
-        methods:{
-            getNominal(month){
-                const data = this.totalPayments.filter(el =>el.month == month)
-                return data.length !== 0  ? data[0].nominal :0
+        data: function () {
+            let result = this.months.map(month => {
+                //  console.log()
+                return this.getNominal(month)
+            })
+            return {
+                chartOptions: {
+                    chart: {
+                        id: "vuechart-example",
+                    },
+                    xaxis: {
+                        categories: this.months,
+                    },
+                    yaxis: {
+                        labels: {
+                            formatter: function (value) {
+                                return numeral(value).format('0.0a');
+                            },
+                            formatter: function (val, opt) {
+                                return numeral(val).format('0.0a')
+                            },
+                        },
+                    },
+                },
+               sparkline: {
+                enabled: true
+                },
+                series: [{
+                    name: "series-1",
+                    data: result,
+                }, ],
+
+            };
+        },
+        methods: {
+            getNominal(month) {
+                const data = this.totalPayments.filter(el => el.month == month)
+                return data.length !== 0 ? data[0].nominal : 0
+            },
+            kFormatter(num) {
+                return Math.abs(num) > 999 ? Math.sign(num) * ((Math.abs(num) / 1000).toFixed(1)) + 'k' : Math.sign(
+                    num) * Math.abs(num)
             }
-            
+
         }
     }
+
 </script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,13 @@ require('./bootstrap');
 import { createApp, h } from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import VueNumerals from 'vue-numerals';
 
 
 const el = document.getElementById('app');
 InertiaProgress.init()
 
-createApp({
+const app = createApp({
     render: () =>
     h(InertiaApp, {
         initialPage: JSON.parse(el.dataset.page),
@@ -18,9 +19,10 @@ createApp({
 })
 .mixin({ methods: { route } })
 .use(InertiaPlugin)
-.mount(el);
+.mount(el); 
 
 // library.add(faUserSecret)
+app.use(VueNumerals); // default locale is 'en'
 
 // h.component('font-awesome-icon', FontAwesomeIcon)
 
